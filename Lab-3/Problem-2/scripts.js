8a4b489f12991ffdc7ee4ebbb8594829af397101
Saul <saul.heno@gmail.com>
2022-12-03 23:25:10 +0000

    styling

--- Lab-3/Problem-2/scripts.js
+++ Lab-3/Problem-2/scripts.js
@@ -8,6 +8,8 @@ const timing = fromEvent(startButton, 'click');
 timing.subscribe(timer);
 
 function timer() {
+    clearInterval(interval)
+    count = 0
     hour = document.getElementById("H").value
     hourInSecs = hour * 3600
     minute = document.getElementById("M").value
@@ -16,14 +18,20 @@ function timer() {
     setTime = hourInSecs + minInSecs + secs
     console.log(hour, minute, secs)
     console.log(setTime)
-    count = 1
     curr = 0;
+    count = 1
     var interval = setInterval(function() {
         console.log("In func")
         curr = curr + 1;
         time = setTime - curr;
         console.log(time);
-        document.getElementById('timeDisplay').innerHTML = time;
+
+        hour = Math.floor(time / 3600)
+        secs = time - (hour * 3600)
+        minute = Math.floor(secs / 60)
+        secs = secs - (minute * 60)
+
+        document.getElementById('timeDisplay').innerHTML = hour + ":" + minute + ":" + secs;
         if( count == 0 || curr == setTime){
             clearInterval(interval)
         }
