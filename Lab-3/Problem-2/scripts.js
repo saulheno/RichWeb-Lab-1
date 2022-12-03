9c2216c7acf6a2bb2e474a15a3ec8d556f1e6ec5
Saul <saul.heno@gmail.com>
2022-12-03 20:42:18 +0000

    Trying basic counter based on waiting. Not working

--- Lab-3/Problem-2/scripts.js
+++ Lab-3/Problem-2/scripts.js
@@ -2,10 +2,23 @@ const { Observable, fromEvent, range, map } = rxjs;
 
 const startButton = document.querySelector(".start");
 
-const time = 10;
+const setTime = 10;
 
 const timing = fromEvent(startButton, 'click');
-const subscription = timing
-    .range(0, time)
-    .map(curr => time - curr)
-    .subscribe(curr => console.log(curr));
+timing.subscribe(timer);
+
+function timer() {
+    console.log("In func")
+    count = 1
+    curr = 0;
+    while( count = 1 && curr != setTime ) {
+        console.log("In while")
+        setTimeout(function() {
+            console.log("In func")
+            curr = curr + 1;
+            time = setTime - curr;
+            console.log(time);
+        }, 1000)
+        
+    }
+}
