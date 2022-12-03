78f752df36c7491a36cbd6610f78cdbaf175e89b
Saul <saul.heno@gmail.com>
2022-12-03 22:55:59 +0000

    Converting all to Secs

--- Lab-3/Problem-2/scripts.js
+++ Lab-3/Problem-2/scripts.js
@@ -2,16 +2,20 @@ const { Observable, fromEvent, range, map } = rxjs;
 
 const startButton = document.querySelector(".start");
 
-const setTime = 0;
+var setTime = 0;
 
 const timing = fromEvent(startButton, 'click');
 timing.subscribe(timer);
 
 function timer() {
     hour = document.getElementById("H").value
+    hourInSecs = hour * 3600
     minute = document.getElementById("M").value
+    minInSecs = minute * 60
     secs = document.getElementById("S").value
+    setTime = hourInSecs + minInSecs + secs
     console.log(hour, minute, secs)
+    console.log(setTime)
     count = 1
     curr = 0;
     var interval = setInterval(function() {
